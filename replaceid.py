dda8cec98dce20a3e4bd94cc606f05ac5f970248
Mike_jj <mkokushin18@gmail.com>
2020-11-12 10:25:02 +0300

    Working

--- replaceid.py
+++ replaceid.py
@@ -3,7 +3,7 @@ from time import time
 from openpyxl import Workbook
 tic = time()
 
-tabs_id = 'test.xlsx'
+tabs_id = 'tabs_elements.xlsx'
 id = 'rez.xlsx'
 
 def gf(file):
@@ -11,58 +11,64 @@ def gf(file):
     ws = wb.active
     return ws
 
+def list_to_string(ll):
+    str = ''
+    for i in ll:
+        str = str + i + ' / '
+
+    return str
+
 old_new_id = {}
 dist_id = {}
 
 
 
-for i in range(2,11):
+for i in range(2,607):
     key_id = str(gf(tabs_id)['A' + str(i)].value)
     dist_id[key_id] = [(str(gf(tabs_id)['C' + str(i)].value)).split('/')]
     dist_id[key_id].append(str(gf(tabs_id)['E' + str(i)].value).split('/'))
     dist_id[key_id].append(str(gf(tabs_id)['F' + str(i)].value).split('/'))
 
-for i in dist_id:
-    print(f'{i} => {dist_id[i]}')
-
 n = 0
 for i in range(2,788):
     key = str(gf(id)['B' + str(i)].value)
     old_new_id[key] = str(gf(id)['C' + str(i)].value)
-    if (n % 80) == 0:
-        print(f'{int(n/8)} %')
-    n = n + 1
+    # if (n % 80) == 0:
+    #     print(f'{int(n/8)} %')
+    # n = n + 1
 print(len(old_new_id))
 
 wb = Workbook()
 sheet = wb.active
-sheet['A1'] = gf(id)['A1'.value]
-sheet['B1'] = gf(id)['C1'.value]
-sheet['C1'] = gf(id)['E1'.value]
-sheet['E1'] = gf(id)['F1'.value]
+sheet['A1'] = 'Name'
+sheet['B1'] = 'Фильтры'
+sheet['C1'] = 'Аксессуары и сопуствующие товары'
+sheet['D1'] = 'Похожие товары'
 n = 2
 for i in dist_id:
-    sheet['A' + str(n)] = i
+    sheet['A'+str(n)] = i
+    pp = 0
     for j in dist_id[i]:
         b =[]
         for k in j:
-
             if k != 'None':
                 p = k
                 s = k.split()
-
                 if s[-1][1:-1] in old_new_id:
                     rs = s[-1][1:-1]
-
                     b.append(k.replace(rs, old_new_id[rs]))
-
-        sheet['B'+str(n)] = b
+        if pp == 0:
+            sheet['B'+str(n)] = list_to_string(b)
+        elif pp == 1:
+            sheet['C'+str(n)] = list_to_string(b)
+        else:
+            sheet['D'+str(n)] = list_to_string(b)
+        pp = pp + 1
     n = n + 1
 
 wb.save('tt.xlsx')
 
-for i in dist_id:
-    print(f'{i} => {dist_id[i]}')
+
 
 print('Ok')
 toc = time()
